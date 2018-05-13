dc3c62f79404b2ebc0c740656b95707e864b085b
john janes <john@johnjanes.com>
2018-05-13 16:00:48 -0500

    refactor

--- create_inventory.sh
+++ create_inventory.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-echo "[prod]" > .inventory
-terraform output prod-web01 >> .inventory
-terraform output prod-db01 >> .inventory
+cd ./terraform/
+echo "[prod]" >  ../ansible/.inventory
+terraform output prod-web01 >> ../ansible/.inventory
+terraform output prod-db01 >> ../ansible/.inventory
 echo "[web]" >> .inventory
-terraform output prod-web01 >> .inventory
+terraform output prod-web01 >> ../ansible/.inventory
 echo "[db]" >> .inventory
-terraform output prod-db01 >> .inventory
+terraform output prod-db01 >> ../ansible/.inventory
